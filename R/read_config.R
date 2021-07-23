6138a8277e7721f49375dcd9a90de129bb2198d2
James Black <james@epijim.uk>
2021-07-23 09:07:34 +0200

    add functions to read the YAMLs

---
+++ R/read_config.R
@@ -0,0 +1,48 @@
+#' Read in the formatted 'config' YAML
+#'
+#' @description
+#' \Sexpr[results=rd, stage=render]{lifecycle::badge("questioning")}
+#' `read_config()` pulls in the config file, that has info like what
+#' are the different roles, and allowable values for some parameters (for CICD).
+#'
+#' @param path The path to the file of interest. Defaults to current dir.
+#' @param file This is likely to always be `config.yaml`, but I was stuck
+#' between calling it config, and 'lookups' - as initially it only contains
+#' lookups.
+#' See the function [enrich_people()] to tidy up the data.
+#' @export
+#'
+
+read_config <- function(
+  path = ".",
+  file = "config.yaml"
+){
+  config <- yaml::read_yaml(file.path(path,file))
+
+  job_levels <- config$job_levels %>%
+    tibble::enframe(.) %>%
+    tidyr::unnest(
+      cols = c(value)
+    ) %>%
+    dplyr::mutate(
+      order = dplyr::row_number()
+    )
+
+  eoy_rating_levels <- tibble::tibble(
+    name = config$draft_eoy_rating
+  ) %>%
+    dplyr::mutate(
+      order = dplyr::row_number()
+    )
+
+  list(
+    "job_levels" = job_levels,
+    "eoy_rating_levels" = eoy_rating_levels
+  )
+}
+
+
+
+
+
+
---
+++ R/read_people.R
@@ -0,0 +1,109 @@
+#' Read in the formatted 'people' YAMLs
+#'
+#' @description
+#' \Sexpr[results=rd, stage=render]{lifecycle::badge("questioning")}
+#' `read_people()` allows you to pull in data from the YAML files that
+#' contain info on your team.
+#'
+#' @param path The path to the files of interest. Defaults to current dir.
+#' @param exclude As it pulls all files ending in `.yaml`, this lets you
+#' exclude certain files that don't contain data, like `config.yaml`.
+#' @return Tibble with info per-person. Itemized info, like highlights
+#' and dev areas are nested vectors within a cell. See
+#' the function [enrich_people()] to tidy up the data.
+#' @export
+#'
+
+read_people <- function(
+  path = ".",
+  exclude = c("config.yaml")
+){
+  files <- list.files(
+    path = path,
+    pattern = "\\.yaml$"
+  )
+
+  files <- setdiff(
+    files,exclude
+  )
+
+  output <- NULL
+  for (file in files) {
+    manager <- gsub(".yaml","",file)
+    manager <- gsub("_"," ",manager)
+    i_manager <- yaml::read_yaml(file.path(path,file))
+    for (person in names(i_manager)) {
+      output <- dplyr::bind_rows(
+        hlp_extract_person(i_manager, person,manager),
+        output
+      )
+    }
+  }
+
+  output
+}
+
+#' Take person from list
+#'
+#' Pulls out person data from list extracted from YAML.
+#'
+#' @param manager_data Data from one managers YAML
+#' @param j Name of the person to extract
+#'
+#' @noRd
+
+hlp_extract_person <- function(manager_data, j, manager){
+  j_all_roles <- manager_data[[j]]$job_levels %>%
+    extract_nested %>%
+    dplyr::mutate(
+      job_level = name,
+      name = j,
+      manager = manager
+    )
+
+  # Make tibble with person data
+  tibble::tibble(
+    name = j,
+    userid = manager_data[[j]]$userid,
+    job_level = j_all_roles %>%
+      dplyr::filter(order == max(order)) %>%
+      dplyr::pull(job_level),
+    started_role = j_all_roles %>%
+      dplyr::filter(order == max(order)) %>%
+      dplyr::pull(value),
+    draft_eoy_rating = manager_data[[j]]$draft_eoy_rating,
+    promotion_readiness = manager_data[[j]]$promotion_readiness,
+
+    highlights = list(manager_data[[j]]$highlights),
+    key_dev_areas = list(manager_data[[j]]$key_dev_areas)
+  )
+}
+
+#' Extract nested
+#'
+#' Takes out the named vector and gives back tibble with order as col
+#'
+#' @param data List from YAML
+#'
+#' @noRd
+extract_nested <- function(data){
+  data %>%
+    tibble::enframe(.) %>%
+    tidyr::unnest(
+      cols = c(value)
+    ) %>%
+    dplyr::mutate(
+      order = dplyr::row_number()
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
